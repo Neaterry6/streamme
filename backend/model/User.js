7f9928a813ad0a6d44c508c745645f7c948641be
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2026-02-18 11:10:59 +0100

    Create User model with username and password fields

---
+++ backend/model/User.js
@@ -0,0 +1,9 @@
+// backend/models/User.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String
+});
+
+module.exports = mongoose.model("User", UserSchema);
